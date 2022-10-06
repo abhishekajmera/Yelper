8e2dad3d9ee606d5c45fde55834b7fd2d09032ae
Abhishek Ajmera <37187598+abhishekajmera@users.noreply.github.com>
2022-10-06 14:44:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,10 +12,6 @@ def hello():
     return app.send_static_file('index.html')
 
 
-# @app.route('/search', methods=["GET"])
-# def do_search():
-#     print('hello')
-
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(debug=True)
