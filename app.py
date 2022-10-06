94ef88bc463deb9a83f1581fef36ab44b78f6b7e
Abhishek Ajmera <37187598+abhishekajmera@users.noreply.github.com>
2022-10-06 15:00:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import requests
+import json
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__, static_folder='static')
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    """Return a friendly HTTP greeting."""
+    return app.send_static_file('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
