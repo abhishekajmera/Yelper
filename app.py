80a96bbd620edad24b43dd69a064a1c35025f42c
Abhishek Ajmera <37187598+abhishekajmera@users.noreply.github.com>
2022-10-06 15:51:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,17 @@
-from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-app = Flask(__name__)
+
+import json
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__, static_folder='static')
 
 
 @app.route('/')
+@app.route('/index')
 def index():
-   print('Request for index page received')
-   return static_file('index.html')
-
-# @app.route('/favicon.ico')
-# def favicon():
-#     return send_from_directory(os.path.join(app.root_path, 'static'),
-#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-# @app.route('/hello', methods=['POST'])
-# def hello():
-#    name = request.form.get('name')
-
-#    if name:
-#        print('Request for hello page received with name=%s' % name)
-#        return render_template('hello.html', name = name)
-#    else:
-#        print('Request for hello page received with no name or blank name -- redirecting')
-#        return redirect(url_for('index'))
+    """Return a friendly HTTP greeting."""
+    return app.send_static_file('index.html')
 
 
 if __name__ == '__main__':
-   app.run()
+    app.run(debug=True)
